36660278b19d58ebce44196a9e86e23eb0976f33
ZhangChuan <zc1415926@126.com>
2016-11-12 01:39:43 +0000

    fix table performance to performance_score, create the migration file

---
+++ database/migrations/2016_11_11_021115_CreatePerformanceScoreTable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePerformanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('performance_score', function(Blueprint $table){
+            $table->increments('id');
+            $table->tinyInteger('delta_score');
+            $table->string('comment');
+            $table->foreign('student_number')
+                  ->references('student_number')
+                  ->on('students')
+                  ->onDelete('cascade');
+            $table->timestamps();
+            
+            $table->index('student_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('performance_score');
+    }
+}
