933863d7268d13ae04af5debfd6ae5e8fd25bec0
ZhangChuan <zc1415926@126.com>
2017-02-10 14:27:38 +0000

    give every student performance score in every term

--- database/seeds/PerformanceTableSeeder.php
+++ database/seeds/PerformanceTableSeeder.php
@@ -22,17 +22,23 @@ class PerformanceTableSeeder extends Seeder
                 {
                     $classCode = (2016 - $i) . '0' . $j;
                     $termCodeArray = ['20150', '20151', '20160', '20161'];
-                    //give a student 1 to 4 record randomly
-                    $recordCount = rand(1, 4);
-                    for($l = 0; $l < $recordCount; $l++)
-                    {
-                        Performance::create([
-                            'delta_score' => rand(-10, 10),
-                            'comment' => str_random(rand(5, 20)),
-                            'student_number' => $classCode . '0' . $k,
-                            'term_code' => $termCodeArray[$recordCount-1]
-                        ]);
+                    
+                    foreach($termCodeArray as $termCode){
+                        
+                        //give a student 1 to 4 record randomly
+                        $recordCount = rand(1, 4);
+                        
+                        for($l = 0; $l < $recordCount; $l++)
+                        {
+                            Performance::create([
+                                'delta_score' => rand(-10, 10),
+                                'comment' => str_random(rand(5, 20)),
+                                'student_number' => $classCode . '0' . $k,
+                                'term_code' => $termCode
+                            ]);
+                        }
                     }
+                    
                     
                 }
             }
